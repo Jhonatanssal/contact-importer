252c38c7ecb5ec51cc566ea66c7fdd85edd709ce
Jhonatan Sarrazola <jhonatan.sarrazola@koombea.com>
2021-11-02 12:48:33 -0500

    Refactor UploaderJob and Tests

--- spec/jobs/files_uploader_job_spec.rb
+++ spec/jobs/files_uploader_job_spec.rb
@@ -3,12 +3,48 @@
 require "rails_helper"
 
 RSpec.describe FilesUploaderJob, type: :job do
+  include ActiveJob::TestHelper
+
   describe "#perform_later" do
+    subject { FilesUploaderJob.perform_later(columns, books, user.id, user_file) }
+
+    before { ActiveJob::Base.queue_adapter = :test }
+
+    let(:user) { create(:user) }
+    let(:user_file) { create(:user_file) }
+    let(:books) do
+      [
+        {
+          contact_name: "Test-1",
+          phone: "(+57) 111 222 33 44",
+          address: "Street 111",
+          email: Faker::Internet.email,
+          date_of_birth: "1999/01/01",
+          creadit_card: Faker::Finance.credit_card(:mastercard),
+          franchise: "maestro"
+        }
+      ]
+    end
+    let(:columns) do
+      {
+        contact_name: "0",
+        email: "3",
+        phone: "1",
+        address: "2",
+        date_of_birth: "4",
+        credit_card: "5",
+        franchise: "6"
+      }
+    end
+
     it "enqueues a new job" do
-      ActiveJob::Base.queue_adapter = :test
-      expect do
-        FilesUploaderJob.perform_later
-      end.to have_enqueued_job
+      expect { subject }.to have_enqueued_job
+    end
+
+    it "calls fileUploader" do
+      expect_any_instance_of(UserFiles::FileUploadService).to receive(:call)
+
+      perform_enqueued_jobs { subject }
     end
   end
 end
